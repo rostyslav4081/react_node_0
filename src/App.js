62bfa78dcf6f379f3c7be3477b18c71b17ae7b54
rostyslav4081 <rostuslav9913@gmail.com>
2021-04-17 00:39:23 +0300

    react + node

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import Axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [movieName,setMovieName] = useState('')
+    const [review,setReview] = useState('')
+    const submitReview = () => {
+        Axios.post("http://localhost:3001/api/insert",{movieName: movieName, movieReview: review}).then(
+            () => {console.log('OK INSERT')})
+
+    };
+    return (
+        <div className="App">
+            <h1>CRUD APP</h1>
+            <div className="form">
+                <label>Movie Name</label>
+                <input type="text" name="movieName" onChange={(event) =>{setMovieName(event.target.value)}}/>
+                <label>Review</label>
+                <input type="text" name="review" onChange={(event) =>{setReview(event.target.value)}}/>
+                <button onClick={submitReview}>Submit</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
